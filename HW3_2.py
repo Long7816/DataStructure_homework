c072b74c000a0d8a257cbbee1c775f3d2837d9ed
Long <165145067+Long7816@users.noreply.github.com>
2026-03-30 14:42:34 +0800

    Update HW3_2.py

--- HW3_2.py
+++ HW3_2.py
@@ -1,1 +1,13 @@
+def two_sum(nums, target):
+    prev_map = {} 
+    for i, n in enumerate(nums):
+        if diff in prev_map:
+            return [prev_map[diff], i]
+        prev_map[n] = i
+    return []
 
+# 測試資料
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print(f"結果索引: {result}") # 輸出: [0, 1]
